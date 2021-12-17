8df71c44c2f2310a1f1765de9a86711e9ed7dfa7
Kyle Haynes <kyle@kylehaynes.com.au>
2021-12-17 13:05:25 +1000

    Update search_data.R

--- R/search_data.R
+++ R/search_data.R
@@ -39,220 +39,262 @@
 # d <- iris
 # d <- data.frame(lapply(d, as.character), stringsAsFactors=FALSE)
 # setDT(d)
+search_data <- function(d) {
+    app <- shinyApp(    
+        ui =
+            fluidPage(
+                #    sidebarLayout(
+                #      sidebarPanel(width = 1, # HTML("This button will open Panel 1 using <code>updateCollapse</code>."),
+                #                   actionButton("p1Button", "Push Me!") #,
+                #                 #   selectInput("styleSelect", "Select style for Panel 1",
+                #                 #    c("default", "primary", "danger", "warning", "info", "success"))
+                #      ),
+                mainPanel(
+                    width = 12,
+                    bsCollapse(
+                        id = "collapseExample", multiple = T, open = c("Setup", "Search", "Results"),
+                        bsCollapsePanel("Setup", "Select / Remove variables you want to view...",
+                            style = "success"
+                            # , "Subset vars:"
+                            , selectInput("sub_vars", NULL, names(d), selected = names(d), multiple = T, selectize = T, width = "50%")
+                        ),
+                        bsCollapsePanel("Search",
+                            style = "info"
+                            # ---- Subset Vars ----
 
-# source("search_data.R")
-# search_data(d)
 
-search_data <- function(d){
+                            # ---- First search
+                            , fluidRow(
+                                column(
+                                    2,
+                                    selectInput("var_1", NULL, names(d), selected = NULL, multiple = T)
+                                ),
+                                column(
+                                    1,
+                                    selectInput("comp_1", NULL, gnaf.r:::comp_types, selected = gnaf.r:::comp_types[5], multiple = F)
+                                ),
+                                column(
+                                    2, # offset = 4,
+                                    textInput("val_1", NULL, value = "", width = NULL, placeholder = NULL)
+                                ),
+                                column(
+                                    3, # offset = 4,
+                                    textOutput("results_1")
+                                )
+                            )
 
-    app = shinyApp(
-    ui =
-    fluidPage(
-    #    sidebarLayout(
-    #      sidebarPanel(width = 1, # HTML("This button will open Panel 1 using <code>updateCollapse</code>."),
-    #                   actionButton("p1Button", "Push Me!") #,
-    #                 #   selectInput("styleSelect", "Select style for Panel 1",
-    #                 #    c("default", "primary", "danger", "warning", "info", "success"))
-    #      ),
-        mainPanel(width = 12,
-        bsCollapse(id = "collapseExample", multiple = T, open = c("Setup", "Search", "Results"),
-                    bsCollapsePanel("Setup", "Select / Remove variables you want to view...", style = "success"
-                        # , "Subset vars:"
-                        , selectInput("sub_vars", NULL, names(d), selected = names(d), multiple = T, selectize = T, width = "50%")
-                    ),
+                            # ---- Second search
+                            , fluidRow(
+                                column(
+                                    2,
+                                    selectInput("var_2", NULL, names(d), selected = NULL, multiple = T)
+                                ),
+                                column(
+                                    1,
+                                    selectInput("comp_2", NULL, gnaf.r:::comp_types, selected = gnaf.r:::comp_types[5], multiple = F)
+                                ),
+                                column(
+                                    2, # offset = 4,
+                                    textInput("val_2", NULL, value = "", width = NULL, placeholder = NULL)
+                                ),
+                                column(
+                                    3, # offset = 4,
+                                    textOutput("results_2")
+                                )
+                            )
 
-                    bsCollapsePanel("Search", style = "info"
-                        # ---- Subset Vars ----
-                        
-                        
-                        # ---- First search
-                        , fluidRow(
-                            column(2,
-                            selectInput("var_1", NULL, names(d), selected = names(d)[1], multiple = T)
+                            # ---- Third search
+                            , fluidRow(
+                                column(
+                                    2,
+                                    selectInput("var_3", NULL, names(d), selected = NULL, multiple = T)
+                                ),
+                                column(
+                                    1,
+                                    selectInput("comp_3", NULL, gnaf.r:::comp_types, selected = gnaf.r:::comp_types[5], multiple = F)
+                                ),
+                                column(
+                                    2, # offset = 4,
+                                    textInput("val_3", NULL, value = "", width = NULL, placeholder = NULL)
+                                ),
+                                column(
+                                    3, # offset = 4,
+                                    textOutput("results_3")
+                                )
                             ),
-                            column(1,
-                            selectInput("comp_1", NULL, gnaf.r:::comp_types, selected = gnaf.r:::comp_types[5], multiple = F)
-                            ),
-                            column(2, # offset = 4,
-                            textInput("val_1", NULL, value = "", width = NULL, placeholder = NULL)
-                            ),
-                            column(3, # offset = 4,
-                            textOutput("results_1")
-                            )      
+                            
+                            # , textInput("threshold", "Threshold:", value = "1", width = NULL, placeholder = NULL)
+                            selectInput("threshold", "Threshold:", as.character(1:10), selected = "1", multiple = F),
+                            selectInput("group_vars", "Group variables:", names(d), multiple = T),
+                            actionButton("action", label = "Search ...")
+
+                            # End of panel 1
+                        ),
+                        bsCollapsePanel(
+                            "Results",
+                            # Panel 3
+                            DT::dataTableOutput("mytable") %>% withSpinner(color = "#0dc5c1")
                         )
+                    )
+                )
+            ),
+        server =
+            function(input, output, session) {
+                #    output$genericPlot <- renderPlot(plot(rnorm(100)))
+                #    observeEvent(input$p1Button, ({
+                #      updateCollapse(session, "collapseExample", open = "Panel 1")
+                #    }))
+                #    observeEvent(input$styleSelect, ({
+                #      updateCollapse(session, "collapseExample", style = list("Panel 1" = input$styleSelect))
+                #    }))
 
-                        # ---- Second search
-                        , fluidRow(
-                            column(2,
-                            selectInput("var_2", NULL, names(d), selected = names(d)[2], multiple = T)
-                            ),
-                            column(1,
-                            selectInput("comp_2", NULL, gnaf.r:::comp_types, selected = gnaf.r:::comp_types[5], multiple = F)
-                            ),
-                            column(2, # offset = 4,
-                            textInput("val_2", NULL, value = "", width = NULL, placeholder = NULL)
-                            ),
-                            column(3, # offset = 4,
-                            textOutput("results_2")  
-                            )    
-                        )
+                #    observeEvent(input$styleSelect, ({
+                #      updateCollapse(session, "collapseExample", style = list("Panel 1" = input$styleSelect))
+                #    }))
 
-                        # ---- Third search
-                        , fluidRow(
-                            column(2,
-                            selectInput("var_3", NULL, names(d), selected = names(d)[3], multiple = T)
-                            ),
-                            column(1,
-                            selectInput("comp_3", NULL, gnaf.r:::comp_types, selected = gnaf.r:::comp_types[5], multiple = F)
-                            ),
-                            column(2, # offset = 4,
-                            textInput("val_3", NULL, value = "", width = NULL, placeholder = NULL)
-                            ),
-                            column(3, # offset = 4,
-                            textOutput("results_3")  
-                            )    
-                        )
-
-                        # , textInput("threshold", "Threshold:", value = "1", width = NULL, placeholder = NULL)
-                        , selectInput("threshold", "Threshold:", as.character(1:10), selected = "1", multiple = F)
-                        , selectInput("group_vars", "Group variables:", names(d), multiple = T)
-                        , actionButton("action", label = "Search ...")
-
-                        # End of panel 1 
-                    ),
-                    bsCollapsePanel("Results", 
-                    # Panel 3                
-                    DT::dataTableOutput("mytable")
-        )
-        )
-    )
-    ),
-    server =
-    function(input, output, session) {
-    #    output$genericPlot <- renderPlot(plot(rnorm(100)))
-    #    observeEvent(input$p1Button, ({
-    #      updateCollapse(session, "collapseExample", open = "Panel 1")
-    #    }))
-    #    observeEvent(input$styleSelect, ({
-    #      updateCollapse(session, "collapseExample", style = list("Panel 1" = input$styleSelect))
-    #    }))
-    
-    #    observeEvent(input$styleSelect, ({
-    #      updateCollapse(session, "collapseExample", style = list("Panel 1" = input$styleSelect))
-    #    }))
-
-
-
-
-        
-        observeEvent(input$action, {
-            # browser()
-            l <- rep(F, nrow(d))
-            d[, l := F]
-
-            if(input$val_1 != ""){
-                res_1 <- c()
-                for(i in 1:length(input$var_1)){
-                    # browser()
-                    d[, l := l + (tmp <<- gnaf.r:::comp(eval(as.name(input$var_1[i])), input$val_1, input$comp_1))]
-                    res_1 <- c(res_1, sum(tmp))
-                }
-                output$results_1 <- renderText({as.character(paste(res_1, collapse = ", "))})
-            }
-
-            if(input$val_2 != ""){
-                res_2 <- c()
-                for(i in 1:length(input$var_2)){
-                    d[, l := l + (tmp <<- gnaf.r:::comp(eval(as.name(input$var_2[i])), input$val_2, input$comp_2))]
-                    res_2 <- c(res_2, sum(tmp))
-                }
-                output$results_2 <- renderText({as.character(paste(res_2, collapse = ", "))})
-            }
-
-            if(input$val_3 != ""){
-                res_3 <- c()
-                for(i in 1:length(input$var_3)){
-                    d[, l := l + (tmp <<- gnaf.r:::comp(eval(as.name(input$var_3[i])), input$val_3, input$comp_3))]
-                    res_3 <- c(res_3, sum(tmp))
-                }
-                output$results_3 <- renderText({as.character(paste(res_3, collapse = ", "))})
-            }
-
-            # Grouping stuff ...
-            gr <- input$group_vars
-            if(length(gr) >= 1){
-                # browser()
-                d[, group_flag := FALSE]
-                for(i in 1:length(gr)){
-                    gids <- d[l >= 1 & !eval(as.name(gr[i])) %in% c("", NA), get(..gr[i])]
-                    d[eval(as.name(gr[i])) %in% gids, group_flag := TRUE]
-                }
-            }
-
-            vars <- input$sub_vars
-            # browser()
-            if(is.null(length(vars))){
-                vars <- names(d)[1:2]
-            }
-
-            if(length(gr) >= 1){
-                output$mytable = DT::renderDataTable({
-                    head(d[(group_flag) | l >= as.numeric(input$threshold), ..vars], 1000)
-                })
+                # Coerce to a data.table
+                if(!is.data.table(d)){
+                    d <- copy(data.table(d))
                 } else {
-                output$mytable = DT::renderDataTable({
-                    head(d[l >= as.numeric(input$threshold), ..vars], 1000)
-                })
+                    d <- copy(d)
                 }
 
-        })
+                if(any(names(d) == "l")){
+                    warnings("var names `l` is reserved for use in this function, it will be overwritten")
+                }
+
+                # Coerce specific columns to characters...
+                for(i in names(d)){
+
+                    if(class(d[[i]])[1] == "character"){
+                        next
+                    } else if(class(d[[i]])[1] %in% c("numeric", "integer", "factor")){
+                        d[, (i) := as.character(get(..i))]
+                    } else if(class(d[[i]])[1] %in% c("Date")){
+
+                        d[, (i) := as.character(format(get(..i), "%Y-%m-%d"))]
+                    } else if(class(d[[i]])[1] %in% c("list")){
+
+                        d[, (i) := as.character(sapply(get(..i), paste, collapse = ", "))]
+                    }
+                }
 
 
-    }
+                observeEvent(input$action, {
+
+                    l <- rep(F, nrow(d))
+                    d[, l := F]
+
+                    if (input$val_1 != "") {
+                        res_1 <- c()
+                        for (i in 1:length(input$var_1)) {
+                            d[, l := l + (tmp <<- gnaf.r:::comp(eval(as.name(input$var_1[i])), input$val_1, input$comp_1))]
+                            res_1 <- c(res_1, sum(tmp))
+                        }
+                        output$results_1 <- renderText({
+                            as.character(paste(res_1, collapse = ", "))
+                        })
+                    }
+
+                    if (input$val_2 != "") {
+                        res_2 <- c()
+                        for (i in 1:length(input$var_2)) {
+                            d[, l := l + (tmp <<- gnaf.r:::comp(eval(as.name(input$var_2[i])), input$val_2, input$comp_2))]
+                            res_2 <- c(res_2, sum(tmp))
+                        }
+                        output$results_2 <- renderText({
+                            as.character(paste(res_2, collapse = ", "))
+                        })
+                    }
+
+                    if (input$val_3 != "") {
+                        res_3 <- c()
+                        for (i in 1:length(input$var_3)) {
+                            d[, l := l + (tmp <<- gnaf.r:::comp(eval(as.name(input$var_3[i])), input$val_3, input$comp_3))]
+                            res_3 <- c(res_3, sum(tmp))
+                        }
+                        output$results_3 <- renderText({
+                            as.character(paste(res_3, collapse = ", "))
+                        })
+                    }
+
+                    # Grouping stuff ...
+                    gr <- input$group_vars
+                    if (length(gr) >= 1) {
+    
+                        d[, group_flag := FALSE]
+                        for (i in 1:length(gr)) {
+                            gids <- d[l >= 1 & !eval(as.name(gr[i])) %in% c("", NA), get(..gr[i])]
+                            d[eval(as.name(gr[i])) %in% gids, group_flag := TRUE]
+                        }
+                    }
+
+                    vars <- input$sub_vars
+                    # browser()
+                    if (is.null(length(vars))) {
+                        vars <- names(d)[1:2]
+                    }
+                    # browser()
+                    updateSelectInput(
+                        inputId = "threshold",
+                        choices = 1:max(d$l),
+                        selected = max(d$l, na.rm = T)
+                    )
+
+                    # browser()
+
+                    if (length(gr) >= 1) {
+                        output$mytable <- DT::renderDataTable({
+                            head(d[(group_flag) | l >= as.numeric(input$threshold), c(..vars, "l")][order(-l)], 10000)
+                        })
+                    } else {
+                        output$mytable <- DT::renderDataTable({
+                            head(d[l >= as.numeric(input$threshold), c(..vars, "l")][order(-l)], 10000)
+                        }) # %>% formatStyle(
+                           #  2,
+                           # backgroundColor = styleEqual(1, c("KYLE JOHN", "ALYSIA MIN HEE"), c('gray', 'yellow'))
+                           # )
+                    }
+
+
+                })
+            }
     )
-runApp(app)
-
+    runApp(app)
 }
 
-## Not run: 
+## Not run:
 
 
 comp_types <- c("==", "%ilike%", "%like%", "%flike%", "%plike%")
-comp <- function(x, y, type = comp_types){
-
-            if(length(type) > 1) type <- type[5]
-
-            if(type == "=="){
-                x == y
-            } else if(type == "%ilike%"){
-                x %ilike% y 
-            } else if(type == "%like%"){
-                x %like% y 
-            } else if(type == "%flike%"){
-                x %like% y 
-            } else if(type == "%plike%"){
-                x %plike% y 
-            } 
-
-        }
+comp <- function(x, y, type = comp_types) {
+    if (length(type) > 1) {
+        type <- type[5]
+    } else if (type == "==") {
+        x == y
+    } else if (type == "%ilike%") {
+        x %ilike% y
+    } else if (type == "%like%") {
+        x %like% y
+    } else if (type == "%flike%") {
+        x %like% y
+    } else if (type == "%plike%") {
+        x %plike% y
+    }
+}
 
 #' @export
-        like <- function (vector, pattern, ignore.case = FALSE, fixed = FALSE, perl = FALSE) 
-        {
-            if (is.factor(vector)) {
-                as.integer(vector) %in% grep(pattern, levels(vector), 
-                    ignore.case = ignore.case, fixed = fixed, perl = perl)
-            }
-            else {
-                grepl(pattern, vector, ignore.case = ignore.case, fixed = fixed, perl = perl)
-            }
-        }
-
+like <- function(vector, pattern, ignore.case = FALSE, fixed = FALSE, perl = FALSE) {
+    if (is.factor(vector)) {
+        as.integer(vector) %in% grep(pattern, levels(vector),
+            ignore.case = ignore.case, fixed = fixed, perl = perl
+        )
+    } else {
+        grepl(pattern, vector, ignore.case = ignore.case, fixed = fixed, perl = perl)
+    }
+}
 
 #' @export
-        `%plike%` <- function (vector, pattern) {
-            like(vector, pattern, perl = TRUE)
-        }
-
-
-x <- search_data
+`%plike%` <- function(vector, pattern) {
+    like(vector, pattern, perl = TRUE)
+}
